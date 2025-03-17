1e108def4b741081f9f2d05746b2c12ee50d3773
Palak.Khurana <palakkhurana06@gmail.com>
2025-03-17 12:17:43 +0530

    Added github Actions workflow

--- src/main/java/com/studentmanagement/HazelCastConfig.java
+++ src/main/java/com/studentmanagement/HazelCastConfig.java
@@ -1,23 +1,23 @@
-package com.studentmanagement;
-
-import com.hazelcast.config.Config;
-import com.hazelcast.config.MapConfig;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class HazelCastConfig {
-    @Bean
-    public Config hazelcastCfg(){
-        Config config=new Config();
-        config.setInstanceName("hazelcast instance")
-                .addMapConfig(
-                        new MapConfig()
-                                .setName("default")
-
-                                .setTimeToLiveSeconds(180)
-                                .setMaxIdleSeconds(30)
-                );
-        return config;
-    }
-}
+//package com.studentmanagement;
+//
+//import com.hazelcast.config.Config;
+//import com.hazelcast.config.MapConfig;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class HazelCastConfig {
+//    @Bean
+//    public Config hazelcastCfg(){
+//        Config config=new Config();
+//        config.setInstanceName("hazelcast instance")
+//                .addMapConfig(
+//                        new MapConfig()
+//                                .setName("default")
+//
+//                                .setTimeToLiveSeconds(180)
+//                                .setMaxIdleSeconds(30)
+//                );
+//        return config;
+//    }
+//}
